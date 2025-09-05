84a9b27a479f227dc7d5be8836535c1e7fca1c95
Saulo de Tarso <saulo.detarso06@yahoo.com.br>
2025-09-04 23:47:42 -0300

    feat: implementar lógica de autenticação JWT (login e registro)

--- authentication.js
+++ authentication.js
@@ -1,14 +1,20 @@
 //authentication
-app.post("/login", (req, res) => {
+
+const fs = require("fs");
+app.post('/login', (req, res, next) => {
     //esse teste abaixo deve ser feito no seu banco de dados
-    if (req.body.user === "luiz" && req.body.password === "123") {
+    if (req.body.user === 'luiz' && req.body.password === '123') {
         //auth ok
-        const id = 1; //esse id viria do banco de dados
-        const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-            expiresIn: parseInt(process.env.JWT_EXPIRES)
+        const userId = 1; //esse id viria do banco de dados
+
+        const privateKey = fs.readFileSync('./private.key', 'utf8');
+        const token = jwt.sign({ userId }, privateKey, {
+            expiresIn: parseInt(process.env.JWT_EXPIRES),
+            algorithm: "RS256"
         });
+
         return res.json({ token: token });
     }
 
-    res.status(401).json({ message: "Invalid credentials!" });
+    res.status(401).json({ message: 'Invalid credentials!' });
 })
