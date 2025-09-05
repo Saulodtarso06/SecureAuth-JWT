6a77b5814ee45618b25c1578cdd43f5f5b85de37
Saulo de Tarso <saulo.detarso06@yahoo.com.br>
2025-09-04 23:48:50 -0300

    update - feat: criar middleware verifyJWT para proteger rotas com validação de token

--- verifyJWT.js
+++ verifyJWT.js
@@ -1,3 +1,4 @@
+
 function verifyJWT(req, res, next) {
     let token = req.headers["authorization"];
     if (!token) return res.status(401).json({ message: "No token provided." });
@@ -6,7 +7,8 @@ function verifyJWT(req, res, next) {
     if (blacklist[token]) return res.status(403).json({ message: "Invalid token." });
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const publicKey = fs.readFileSync("./public.key", "utf8");
+        const decoded = jwt.verify(token, publicKey, { algorithm: ["RS256"] });
         if (!decoded) return res.status(403).json({ message: "Invalid token." });
 
         res.locals.token = decoded;
